b26a1d25a5dd4263a1346c201b91b1fdf4f1a7dc
Emilio kariuki <emilio113kariuki@gmail.com>
2022-07-20 01:52:02 +0300

    added path

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hives/Model/UserModel.dart';
-
+import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'View/Home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
   await Hive.initFlutter(); //intialize hive
   Hive.registerAdapter(UserModelAdapter()); //register adapter
   await Hive.openBox<UserModel>('task');
